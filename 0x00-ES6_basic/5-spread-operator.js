e6f31d15eea32b3ffe1a481df5bab292d0b4f669
tamerkat <mahad10taha@gmail.com>
2024-07-22 21:34:37 +0300

    add

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  const arr = array1.concat(array2)
-  arr.push(string.splite(''));
-  return arr
+  return [...array1, ...array2, string]
 }
