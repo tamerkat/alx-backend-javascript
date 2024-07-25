70130a6a80fef44120477c87a160b842e35f5cf2
tamerkat <mahad10taha@gmail.com>
2024-07-25 15:27:48 +0300

    add

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...item,
+    ];
+  }
+  
+  return allEmployees;
+}
